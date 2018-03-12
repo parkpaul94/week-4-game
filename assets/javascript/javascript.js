14fb9b525f5e4e1129fa96150934656e3b45ffd7
Paul Park <park.paul.94@gmail.com>
2018-03-12 03:28:30 -0400

    finished basic outline for the HTML, added and edited CSS files, started on the basic JS including basic global variables, set names and stats for characters and created basic functions

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,55 @@
+var fighter = [];
+var selectedfighter = false;
+var defender = [];
+var selecteddefender = false;
+var fightwon = 0;
+var gameover = false;
+
+var darth   = {
+                name: "Darth Vader",
+                health: 500,
+                baseAttack: 20,
+                damage: 20
+                },
+    boba    = {
+                name: "Boba Fett",
+                health: 300,
+                baseAttack: 8,
+                damage: 8
+    },
+    yoda    = {
+                name: "Yoda",
+                health: 400,
+                baseAttack: 15,
+                damage: 15
+    },
+    luke    = {
+                name: "Luke Skywalker",
+                health: 250,
+                baseAttack: 10,
+                damage: 10
+    };
+
+function setCharacters (cfighter) {
+  fighter.name          = cfighter.name;
+  fighter.health        = cfighter.health;
+  fighter.baseAttack    = cfighter.baseAttack;
+  fighter.attack        = cfighter.attack;
+}
+
+function setDefender (cdefender) {
+    defender.name       = cdefender.name;
+    defender.health     = cdefender.health;
+    defender.baseAttack = cdefender.baseAttack;
+    defender.attack     = cdefender.attack;
+}
+
+function removeCharacter () {
+    $(".available").removeClass("available").addClass("unavailable");
+}
+
+// let health = document.getElementById("health")
+// health.value -= 30; 
+
+// TRY TO MAKE IT SO FIGHTER'S HEALTH INCREASES WITH EVERY BATTLE WON
+// POSSIBLY RANDOMIZE ATTACK DAMAGE?
